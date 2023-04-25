3d6efadbf7b0c03410fd900071e2ab98f0181ae1
Dmitry Kann <yktooo@gmail.com>
2023-04-25 19:27:18 +0200

    WIP: new database model pt. 4

--- db/0001-comentario-v3.sql
+++ db/0001-comentario-v3.sql
@@ -74,7 +74,7 @@ insert into cm_identity_providers(id, name, icon) values
 -- Users
 ------------------------------------------------------------------------------------------------------------------------
 create table cm_users (
-    id             uuid primary key,                            -- Unique user ID
+    id             uuid primary key,                            -- Unique record ID
     email          varchar(254)                not null unique, -- Unique user email
     name           varchar(63)                 not null,        -- User's full name
     password_hash  varchar(100)                not null,        -- Password hash
@@ -97,7 +97,7 @@ create table cm_users (
     website_url    varchar(2083)                                -- Optional user's website URL
 );
 
--- Foreign keys
+-- Constraints
 alter table cm_users add constraint fk_users_user_created  foreign key (user_created)  references cm_users(id) on delete set null;
 alter table cm_users add constraint fk_users_user_banned   foreign key (user_banned)   references cm_users(id) on delete set null;
 alter table cm_users add constraint fk_users_federated_idp foreign key (federated_idp) references cm_identity_providers(id) on delete restrict;
@@ -111,7 +111,7 @@ insert into cm_users(id, email, name, password_hash, system_account, confirmed, 
 -- User sessions
 ------------------------------------------------------------------------------------------------------------------------
 create table cm_user_sessions (
-    id                 uuid primary key,                 -- Unique session ID
+    id                 uuid primary key,                 -- Unique record ID
     user_id            uuid                    not null, -- Reference to the user who owns the session
     ts_created         timestamp               not null, -- When the session was created
     ts_expires         timestamp               not null, -- When the session expires
@@ -126,7 +126,7 @@ create table cm_user_sessions (
     ua_device          varchar(63)  default '' not null  -- User's device type
 );
 
--- Foreign keys
+-- Constraints
 alter table cm_user_sessions add constraint fk_user_sessions_user_id foreign key (user_id) references cm_users(id) on delete cascade;
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -140,8 +140,74 @@ create table cm_tokens (
     multiuse   boolean               -- Whether the token is to be kept until expired; if false, the token gets deleted after first use
 );
 
--- Foreign keys
+-- Constraints
 alter table cm_tokens add constraint fk_tokens_user_id foreign key (user_id) references cm_users(id) on delete cascade;
+
+------------------------------------------------------------------------------------------------------------------------
+-- Domains
+------------------------------------------------------------------------------------------------------------------------
+
+create table cm_domains (
+    id                uuid primary key,                      -- Unique record ID
+    name              varchar(255)          not null,        -- Domain display name
+    host              varchar(259)          not null unique, -- Domain host
+    ts_created        timestamp             not null,        -- When the domain was created
+    is_readonly          boolean default false not null,        -- Whether the domain is readonly (no new comments are allowed)
+    auth_anonymous    boolean default false not null,        -- Whether anonymous comments are allowed
+    auth_local        boolean default false not null,        -- Whether local authentication is allowed
+    auth_sso          boolean default false not null,        -- Whether SSO authentication is allowed
+    sso_url           varchar(2083),                         -- SSO provider URL
+    sso_secret        char(64),                              -- SSO secret
+    moderation_policy varchar(16)           not null,        -- Moderation policy for domain: 'none', 'anonymous', 'all'
+    mod_notify_policy varchar(16)           not null,        -- Moderator notification policy for domain: 'none', 'pending', 'all'
+    default_sort      char(2)               not null,        -- Default comment sorting for domain. 1st letter: s = score, t = timestamp; 2nd letter: a = asc, d = desc
+    count_comments    integer default 0     not null,        -- Total number of comments
+    count_views       integer default 0     not null         -- Total number of views
+);
+
+-- Links between domains and users
+create table cm_domains_users (
+    domain_id        uuid                  not null, -- Reference to the domain
+    user_id          uuid                  not null, -- Reference to the user
+    is_owner         boolean default false not null, -- Whether the user is an owner of the domain (assumes is_moderator and is_commenter)
+    is_moderator     boolean default false not null, -- Whether the user is a moderator of the domain (assumes is_commenter)
+    is_commenter     boolean default false not null, -- Whether the user is a commenter of the domain (if false, the user is readonly on the domain)
+    notify_replies   boolean default true  not null, -- Whether the user is to be notified about replies to their comments
+    notify_moderator boolean default true  not null  -- Whether the user is to receive moderator notifications (only when is_moderator is true)
+);
+
+-- Constraints
+alter table cm_domains_users add primary key (domain_id, user_id);
+alter table cm_domains_users add constraint fk_domains_users_domain_id foreign key (domain_id) references cm_domains(id) on delete cascade;
+alter table cm_domains_users add constraint fk_domains_users_user_id   foreign key (user_id)   references cm_users(id)   on delete cascade;
+
+-- Links between domains and identity providers, specifying which IdPs are allowed on the domain
+create table cm_domains_idps (
+    domain_id uuid        not null, -- Reference to the domain
+    idp_id    varchar(32) not null  -- Reference to the identity provider
+);
+
+-- Constraints
+alter table cm_domains_idps add constraint fk_domains_idps_domain_id foreign key (domain_id) references cm_domains(id)            on delete cascade;
+alter table cm_domains_idps add constraint fk_domains_idps_idp_id    foreign key (idp_id)    references cm_identity_providers(id) on delete cascade;
+
+------------------------------------------------------------------------------------------------------------------------
+-- Domain pages
+------------------------------------------------------------------------------------------------------------------------
+
+create table cm_domain_pages (
+    id             uuid primary key,                    -- Unique record ID
+    domain_id      uuid                       not null, -- Reference to the domain
+    path           varchar(2083)              not null, -- Page path
+    title          varchar(100) default ''    not null, -- Page title
+    is_readonly    boolean      default false not null, -- Whether the page is readonly (no new comments are allowed)
+    count_comments integer      default 0     not null, -- Total number of comments
+    count_views    integer      default 0     not null  -- Total number of views
+);
+
+-- Constraints
+alter table cm_domain_pages add constraint fk_domain_pages_domain_id      foreign key (domain_id) references cm_domains(id) on delete cascade;
+alter table cm_domain_pages add constraint uk_domain_pages_domain_id_path unique (domain_id, path);
 
 --======================================================================================================================
 -- Migrate the legacy schema
@@ -158,23 +224,81 @@ begin
         create temporary table temp_commenterhex_map(commenterhex varchar(64) primary key, id uuid not null unique);
         insert into temp_commenterhex_map(commenterhex, id) select commenterhex, gen_random_uuid() from commenters;
 
+        -- Create a domain mapping table
+        create temporary table temp_domain_map(domain varchar(259) primary key, id uuid not null unique);
+        insert into temp_domain_map(domain, id) select domain, gen_random_uuid() from domains;
+
         -- Migrate owners
         insert into cm_users(id, email, name, password_hash, confirmed, ts_confirmed, ts_created, remarks)
             select m.id, o.email, o.name, o.passwordhash, o.confirmedemail='true', o.joindate, o.joindate, 'Migrated from Commento, ownerhex=' || o.ownerhex
                 from owners o
-            join temp_ownerhex_map m on m.ownerhex=o.ownerhex;
+                join temp_ownerhex_map m on m.ownerhex=o.ownerhex;
 
         -- Migrate commenters
         insert into cm_users(id, email, name, password_hash, confirmed, ts_confirmed, ts_created, remarks, federated_idp, website_url)
-        select
-                m.id, c.email, c.name, c.passwordhash, true, c.joindate, c.joindate,
-                'Migrated from Commento, commenterhex=' || c.commenterhex,
-                case when c.provider='commento' then null else c.provider end,
-                case when c.link='undefined' then null else c.link end
-            from commenters c
-        join temp_commenterhex_map m on m.commenterhex=c.commenterhex
-        -- Prevent adding commenter for an already registered owner user
-        where not exists(select 1 from cm_users u where u.email=c.email);
+            select
+                    m.id, c.email, c.name, c.passwordhash, true, c.joindate, c.joindate,
+                    'Migrated from Commento, commenterhex=' || c.commenterhex,
+                    case when c.provider='commento' then null else c.provider end,
+                    case when c.link='undefined' then null else c.link end
+                from commenters c
+                join temp_commenterhex_map m on m.commenterhex=c.commenterhex
+                -- Prevent adding commenter for an already registered owner user
+                where not exists(select 1 from cm_users u where u.email=c.email);
+
+        -- Migrate domains
+        insert into cm_domains(
+                id, name, host, ts_created, is_readonly, auth_anonymous, auth_local, auth_sso, sso_url, sso_secret, moderation_policy, mod_notify_policy, default_sort)
+            select
+                    m.id, d.name, d.domain, d.creationdate, d.state='frozen', !d.requireidentification, d.commentoprovider, d.ssoprovider, d.ssourl, d.ssosecret,
+                    case
+                        when d.requiremoderation then 'all'
+                        when d.moderateallanonymous then 'anonymous'
+                        else 'none'
+                    end,
+                    case
+                        when d.emailnotificationpolicy='all' then 'all'
+                        when d.emailnotificationpolicy='none' then 'none'
+                        else 'pending'
+                    end,
+                    case
+                        when d.defaultsortpolicy='score-desc' then 'sd'
+                        when d.defaultsortpolicy='creationdate-desc' then 'td'
+                        else 'ta'
+                    end
+                from domains d
+                join temp_domain_map m on m.domain=d.domain;
+
+        -- Migrate domain owners
+        insert into cm_domains_users(domain_id, user_id, is_owner, is_moderator, is_commenter, notify_replies, notify_moderator)
+            select dm.id, om.id, true, true, true, coalesce(e.sendreplynotifications, false), coalesce(e.sendmoderatornotifications, false)
+                from domains d
+                join owners o on o.ownerhex=d.ownerhex
+                join temp_ownerhex_map om on om.ownerhex=o.ownerhex
+                join temp_domain_map dm on dm.domain=d.domain
+                left join emails e on e.email=o.email;
+
+        -- Migrate domain moderators
+        insert into cm_domains_users(domain_id, user_id, is_owner, is_moderator, is_commenter, notify_replies, notify_moderator)
+            select dm.id, u.id, false, true, true, coalesce(e.sendreplynotifications, false), coalesce(e.sendmoderatornotifications, false)
+                from moderators mod
+                join cm_users u on u.email=mod.email
+                join temp_domain_map dm on dm.domain=mod.domain
+                left join emails e on e.email=o.email
+                -- Exclude already migrated owners
+                where not exists(select 1 from cm_users xu where xu.email=mod.email);
+
+        -- Migrate pages
+        insert into cm_domain_pages(id, domain_id, path, title, is_readonly, count_comments)
+            select gen_random_uuid(), m.id, p.path, p.title, p.islocked, p.commentcount
+                from pages p
+                join temp_domain_map m on m.domain=p.domain;
+
+
+        -- TODO update domains.count_comments, count_views
+        -- TODO update cm_domain_pages.count_views
+        -- TODO migrate sticky comments
+
     end if;
 end $$;
 
