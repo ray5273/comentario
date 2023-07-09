e6ca7263dea00291cf9f65d3b216d8285ac02845
Dmitry Kann <yktooo@gmail.com>
2023-07-09 08:49:27 +0200

    fe: WIP: comment moderation buttons

--- frontend/app/_modules/manage/comments/comment-list/comment-list.component.ts
+++ frontend/app/_modules/manage/comments/comment-list/comment-list.component.ts
@@ -2,12 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, merge, tap } from 'rxjs';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { ApiGeneralService, Domain, Comment, Commenter } from '../../../../../generated-api';
+import { ApiGeneralService, Domain, Comment, Commenter, DomainUser, Principal } from '../../../../../generated-api';
 import { DomainSelectorService } from '../../_services/domain-selector.service';
 import { ConfigService } from '../../../../_services/config.service';
 import { Sort } from '../../_models/sort';
 import { ProcessingStatus } from '../../../../_utils/processing-status';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faLink, faTrashAlt, faUser, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @UntilDestroy()
 @Component({
@@ -22,11 +22,20 @@ export class CommentListComponent implements OnInit {
     @Input()
     pageId?: string;
 
-    /** Domain currently selected in domain selector. */
+    /** Logged-in principal. */
+    principal?: Principal;
+
+    /** Currently selected domain. */
     domain?: Domain;
+
+    /** User in the currently selected domain. */
+    domainUser?: DomainUser;
 
     /** Loaded list of comments. */
     comments?: Comment[];
+
+    /** Whether the current user is a moderator, an owner of the domain, or a superuser. */
+    isModerator = false;
 
     /** Whether there are more results to load. */
     canLoadMore = true;
@@ -42,7 +51,11 @@ export class CommentListComponent implements OnInit {
     });
 
     // Icons
-    readonly faUser = faUser;
+    readonly faCheck    = faCheck;
+    readonly faLink     = faLink;
+    readonly faTrashAlt = faTrashAlt;
+    readonly faUser     = faUser;
+    readonly faXmark    = faXmark;
 
     private loadedPageNum = 0;
 
@@ -61,9 +74,17 @@ export class CommentListComponent implements OnInit {
         // Load comment list
         this.load(true);
 
-        // Subscribe to sort/filter changes
+        // Subscribe to domain/sort/filter changes
         merge(
-            this.domainSelectorSvc.domain.pipe(untilDestroyed(this), tap(d => this.domain = d)),
+            this.domainSelectorSvc.domainUserIdps.pipe(
+                untilDestroyed(this),
+                // Store the domain and the user
+                tap(data => {
+                    this.principal  = data.principal;
+                    this.domainUser = data.domainUser;
+                    this.domain     = data.domain;
+                    this.isModerator = !!(this.principal?.isSuperuser || this.domainUser?.isOwner || this.domainUser?.isModerator);
+                })),
             this.sort.changes.pipe(untilDestroyed(this)),
             this.ctlFilterFilter.valueChanges.pipe(untilDestroyed(this), debounceTime(500), distinctUntilChanged()))
             .subscribe(() => this.load(true));
@@ -92,5 +113,21 @@ export class CommentListComponent implements OnInit {
                 // Make a map of user ID => commenter
                 r.commenters?.forEach(cr => this.commenters.set(cr.id!, cr));
             });
+    }
+
+    deleteComment(c: Comment) {
+        // TODO new-db
+    }
+
+    commentLink(c: Comment) {
+        // TODO new-db
+    }
+
+    approveComment(c: Comment) {
+        // TODO new-db
+    }
+
+    rejectComment(c: Comment) {
+        // TODO new-db
     }
 }
