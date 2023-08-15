89b52f1743f5a82ec0e9e8bab83146dda78a7add
Dmitry Kann <yktooo@gmail.com>
2023-08-15 14:12:09 +0200

    instance config: add locking for thread safety

--- internal/svc/config_svc.go
+++ internal/svc/config_svc.go
@@ -6,6 +6,7 @@ import (
 	"github.com/doug-martin/goqu/v9"
 	"github.com/google/uuid"
 	"gitlab.com/comentario/comentario/internal/data"
+	"sync"
 	"time"
 )
 
@@ -28,29 +29,25 @@ type ConfigService interface {
 
 // configService is a blueprint ConfigService implementation
 type configService struct {
+	mu    sync.RWMutex
 	items map[data.InstanceConfigItemKey]*data.InstanceConfigItem
 }
 
 func (svc *configService) Get(key data.InstanceConfigItemKey) (*data.InstanceConfigItem, error) {
 	logger.Debugf("configService.Get(%q)", key)
 
-	// Make sure the config is initialised
-	if svc.items == nil {
-		return nil, errors.New("config is not initialised")
-	}
-
-	// Lookup the key
-	if ci, ok := svc.items[key]; !ok {
-		return nil, fmt.Errorf("config key %q is unknown", key)
-
-	} else {
-		// Succeeded
-		return ci, nil
-	}
+	// Prevent concurrent write access
+	svc.mu.RLock()
+	defer svc.mu.RUnlock()
+	return svc.get(key)
 }
 
 func (svc *configService) Load() error {
 	logger.Debug("configService.Load()")
+
+	// Prevent concurrent access
+	svc.mu.Lock()
+	defer svc.mu.Unlock()
 
 	// Init the config to its defaults
 	svc.reset()
@@ -98,6 +95,10 @@ func (svc *configService) Load() error {
 func (svc *configService) Save() error {
 	logger.Debug("configService.Save()")
 
+	// Prevent concurrent access
+	svc.mu.Lock()
+	defer svc.mu.Unlock()
+
 	// Don't bother if isn't initialised
 	if svc.items == nil {
 		return nil
@@ -143,8 +144,12 @@ func (svc *configService) Save() error {
 func (svc *configService) Set(curUserID *uuid.UUID, key data.InstanceConfigItemKey, value string) error {
 	logger.Debugf("configService.Set(%s, %q, %q)", curUserID, key, value)
 
+	// Prevent concurrent access
+	svc.mu.Lock()
+	defer svc.mu.Unlock()
+
 	// Find the item
-	ci, err := svc.Get(key)
+	ci, err := svc.get(key)
 	if err != nil {
 		return err
 	}
@@ -156,6 +161,23 @@ func (svc *configService) Set(curUserID *uuid.UUID, key data.InstanceConfigItemK
 
 	// Succeeded
 	return nil
+}
+
+// get returns a configuration entry by its key, without locking
+func (svc *configService) get(key data.InstanceConfigItemKey) (*data.InstanceConfigItem, error) {
+	// Make sure the config is initialised
+	if svc.items == nil {
+		return nil, errors.New("config is not initialised")
+	}
+
+	// Lookup the key
+	if ci, ok := svc.items[key]; !ok {
+		return nil, fmt.Errorf("config key %q is unknown", key)
+
+	} else {
+		// Succeeded
+		return ci, nil
+	}
 }
 
 // reset the configuration to its defaults
