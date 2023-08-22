d39ce04e262c464f2c87245d613269e226c88d43
Dmitry Kann <yktooo@gmail.com>
2023-08-22 14:13:54 +0200

    embed: improve markdown help

--- embed/src/markdown-help.ts
+++ embed/src/markdown-help.ts
@@ -24,11 +24,19 @@ export class MarkdownHelp extends Dialog {
                     .classes('table')
                     .append(
                         this.row('<i>italics</i>',                            'Surround text with <code>*asterisks*</code>'),
-                        this.row('<b>bold</b>',                               'Surround text with <code>**two asterisks**</code>'),
+                        this.row('<b>bold</b>',                               'Surround text with <code>**double asterisks**</code>'),
                         this.row('<code>code</code>',                         'Surround text with <code>`backticks`</code>'),
-                        this.row('<del>strikethrough</del>',                  'Surround text with <code>~~two tilde characters~~</code>'),
+                        this.row('<del>strikethrough</del>',                  'Surround text with <code>~~double tildes~~</code>'),
                         this.row('<a href="https://comentario.app">link</a>', '<code>[link](https://comentario.app)</code> or just a bare URL'),
-                        this.row('<blockquote>quote</blockquote>',            'Prefix with <code>&gt;</code>')));
+                        this.row('<blockquote>quote</blockquote>',            'Prefix with <code>&gt;</code>'),
+                        this.row('<ul><li>list</li></ul>',                    'Prefix with <code>*</code>'),
+                    ),
+                UIToolkit.div('dialog-centered')
+                    .append(
+                        Wrap.new('a')
+                            .inner('Read more about Markdown')
+                            // TODO this link doesn't work yet
+                            .attr({href: 'https://docs.comentario.app/en/kb/markdown', target: '_blank'})));
     }
 
     private row(md: string, text: string): Wrap<any> {
