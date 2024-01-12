5eb8ef77a6528975405db1fd64e72c85a50c9c61
Dmitry Kann <yktooo@gmail.com>
2024-01-12 17:48:03 +0100

    e2e: fix failing tests

--- cypress/e2e/embed/profile-bar.cy.ts
+++ cypress/e2e/embed/profile-bar.cy.ts
@@ -1,14 +1,22 @@
 import { DOMAINS, TEST_PATHS, USERS } from '../../support/cy-utils';
 import { EmbedUtils } from '../../support/cy-embed-utils';
 
+// eslint-disable-next-line no-only-or-skip-tests/no-skip-tests
+const itHeaded = Cypress.browser.isHeaded ? it : it.skip;
+
 context('Profile bar', () => {
 
     beforeEach(cy.backendReset);
 
     context('Login button', () => {
 
-        it('triggers SSO auth when only interactive SSO is enabled', () => {
-            cy.backendPatchDomain(DOMAINS.localhost.id, {authLocal: false, authAnonymous: false, ssoNonInteractive: false});
+        // Because of the login popup, this only works with a "headed" browser
+        itHeaded('triggers SSO auth when only interactive SSO is enabled', () => {
+            cy.backendPatchDomain(DOMAINS.localhost.id, {
+                authLocal: false,
+                authAnonymous: false,
+                ssoNonInteractive: false
+            });
             cy.backendUpdateDomainIdps(DOMAINS.localhost.id, []);
             cy.testSiteVisit(TEST_PATHS.comments);
             EmbedUtils.makeAliases({anonymous: true});
@@ -19,19 +27,31 @@ context('Profile bar', () => {
         });
 
         [
-            {name: 'only non-interactive SSO is enabled', props: {authLocal: false, authAnonymous: false}},
-            {name: 'only anonymous is enabled',           props: {authLocal: false, authSso: false}},
-            {name: 'no auth is enabled',                  props: {authLocal: false, authAnonymous: false, authSso: false}},
+            {
+                name:     'only non-interactive SSO is enabled',
+                readonly: false,
+                notice:   undefined,
+                props:    {authLocal: false, authAnonymous: false}},
+            {
+                name:     'only anonymous is enabled',
+                readonly: false,
+                notice:   undefined,
+                props:    {authLocal: false, authSso: false}},
+            {
+                name:     'no auth is enabled',
+                readonly: true,
+                notice:   'This domain has no authentication method available. You cannot add new comments.',
+                props:    {authLocal: false, authAnonymous: false, authSso: false}},
         ]
             .forEach(test =>
-                it(`'isn't available when ${test.name}`, () => {
+                it(`isn't available when ${test.name}`, () => {
                     // Update domain auth props
                     cy.backendPatchDomain(DOMAINS.localhost.id, test.props);
                     // Disable all federated providers
                     cy.backendUpdateDomainIdps(DOMAINS.localhost.id, []);
                     // Visit and check the test site
                     cy.testSiteVisit(TEST_PATHS.comments);
-                    EmbedUtils.makeAliases({anonymous: true, login: false});
+                    EmbedUtils.makeAliases({anonymous: true, login: false, readonly: test.readonly, notice: test.notice});
                 }));
     });
 
