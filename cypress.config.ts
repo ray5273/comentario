36203f22631c1361a0f46aec18ab7de0cc4b53f5
Dmitry Kann <yktooo@gmail.com>
2024-03-14 14:24:41 +0100

    e2e/ci: retry single tests, not whole job

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,5 +8,11 @@ export default defineConfig({
         // Backend's (API and the admin app) base URL
         baseUrl: 'http://localhost:8080',
         numTestsKeptInMemory: 2,
+
+        // Occasionally tests fail for no conceivable reason (most likely due to Cypress/browser quirks). Therefore we
+        // want to retry at least once
+        retries: {
+            runMode: 1,
+        },
     },
 });
