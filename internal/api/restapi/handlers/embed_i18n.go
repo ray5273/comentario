473f1c8bc0836437773a6a4eba26150afffed2c0
Func <Funcer@outlook.com>
2024-09-09 04:24:45 +0800

    embed: serve messages with fallbacks applied (resloves #121)

--- internal/api/restapi/handlers/embed_i18n.go
+++ internal/api/restapi/handlers/embed_i18n.go
@@ -1,36 +1,21 @@
 package handlers
 
 import (
-	"errors"
-	"fmt"
 	"github.com/go-openapi/runtime/middleware"
 	"gitlab.com/comentario/comentario/internal/api/exmodels"
 	"gitlab.com/comentario/comentario/internal/api/restapi/operations/api_embed"
-	"gitlab.com/comentario/comentario/internal/config"
 	"gitlab.com/comentario/comentario/internal/svc"
-	"gitlab.com/comentario/comentario/internal/util"
 )
 
 func EmbedI18nMessages(params api_embed.EmbedI18nMessagesParams) middleware.Responder {
-	// Fetch the messages
-	ms, err := svc.TheI18nService.Messages(params.Lang)
-
-	// If no requested language found
-	if errors.Is(err, svc.ErrNotFound) {
-		// Redirect to the default language instead
-		return api_embed.NewEmbedI18nMessagesTemporaryRedirect().
-			WithLocation(config.ServerConfig.URLForAPI(fmt.Sprintf("/embed/i18n/%s/messages", util.DefaultLanguage), nil))
-
-	} else if err != nil {
-		// Any other error
-		return respServiceError(err)
-	}
+	// Fetch the messages with fallback
+	ms, lang := svc.TheI18nService.Messages(params.Lang)
 
 	// Convert the source message map into an API map
 	mm := exmodels.I18nMessageMap(ms)
 
-	// Let the client know what language we are serving, in case of a redirect
-	mm["_lang"] = params.Lang
+	// Let the client know what language we are serving, in case of unsupported languages or fallbacks
+	mm["_lang"] = lang
 
 	// Succeeded
 	return api_embed.NewEmbedI18nMessagesOK().WithPayload(mm)
