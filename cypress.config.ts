760db34a158d41e89a6186d8ff4ac73a0fc58f93
Dmitry Kann <yktooo@gmail.com>
2025-04-25 11:37:29 +0200

    e2e: add cypress-terminal-report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,26 @@
 import { defineConfig } from 'cypress';
+import * as installLogsPrinter from "cypress-terminal-report/src/installLogsPrinter";
 
 export default defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
-            // implement node event listeners here
+            // Enable the cypress-terminal-report log printer
+            installLogsPrinter(on, {
+                printLogsToConsole: 'onFail',
+                printLogsToFile:    'always',
+                outputRoot:         config.projectRoot + '/cypress/reports/',
+                outputTarget: {
+                    'log.html': 'html',
+                    'log.txt':  'txt',
+                },
+            });
         },
+
         // Backend's (API and the admin app) base URL
         baseUrl: 'http://localhost:8080',
         numTestsKeptInMemory: 2,
 
-        // Occasionally tests fail for no conceivable reason (most likely due to Cypress/browser quirks). Therefore we
+        // Occasionally, tests fail for no conceivable reason (most likely due to Cypress/browser quirks). Therefore we
         // want to retry at least once
         retries: {
             runMode: 1,
